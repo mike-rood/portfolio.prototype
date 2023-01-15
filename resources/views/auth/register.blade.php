84c2dc914538c9fa194b27ed623153958743529a
mike-rood <tomodach84@gmail.com>
2023-01-15 18:36:54 +0300

    Setting up Fortify

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,10 @@
 <x-layouts.app>
 <h1>Register</h1>
+    <form action="{{ route('register') }}" method="POST">
+        @csrf
+        <div><input type="text" name="name" placeholder="Name"></div>
+        <div><input type="email" name="email" placeholder="Email"></div>
+        <div><input type="password" name="password" placeholder="Password"></div>
+        <div><input type="submit" value="Register"></div>
+    </form>
 </x-layouts.app>
